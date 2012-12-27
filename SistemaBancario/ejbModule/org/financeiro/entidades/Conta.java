381fde0123a2ff5d87846b3211eb8498e48a7197
Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>
2012-12-26 23:44:40 -0200

    Signed-off-by: Paulo Henrique Lerbach Rodrigues <palerique@gmail.com>

--- SistemaBancario/ejbModule/org/financeiro/entidades/Conta.java
+++ SistemaBancario/ejbModule/org/financeiro/entidades/Conta.java
@@ -14,7 +14,7 @@ import javax.persistence.Transient;
 public class Conta implements Serializable {
 
 	private static final long serialVersionUID = 8495711620466137809L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
@@ -22,45 +22,56 @@ public class Conta implements Serializable {
 	private String digitoVerificador;
 	private Double saldoAtual;
 	private TipoDeConta tipoDeConta;
-	
+
 	@Transient
 	private Correntista correntista;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNumero() {
 		return numero;
 	}
+
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
+
 	public String getDigitoVerificador() {
 		return digitoVerificador;
 	}
+
 	public void setDigitoVerificador(String digitoVerificador) {
 		this.digitoVerificador = digitoVerificador;
 	}
+
 	public Double getSaldoAtual() {
 		return saldoAtual;
 	}
+
 	public void setSaldoAtual(Double saldoAtual) {
 		this.saldoAtual = saldoAtual;
 	}
+
 	public TipoDeConta getTipoDeConta() {
 		return tipoDeConta;
 	}
+
 	public void setTipoDeConta(TipoDeConta tipoDeConta) {
 		this.tipoDeConta = tipoDeConta;
 	}
+
 	public Correntista getCorrentista() {
 		return correntista;
 	}
+
 	public void setCorrentista(Correntista correntista) {
 		this.correntista = correntista;
 	}
-	
+
 }
